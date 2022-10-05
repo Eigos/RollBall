141a1cf4da79606f5dde258fc0f25b4f0553b142
Eigo <yuzaki.ergin@gmail.com>
2022-10-05 20:34:56 +0300

    .

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -3,17 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BallMovement : MonoBehaviour
-
-
 {
+
+    public float verticalMovementRange = 0;
+    public float verticalMovementSpeed = 0;
+    public float rotationSpeed = 0;
+
+    private Vector3 verticalDestinationPosition = Vector3.zero;
+    private Vector3 verticalStartPosition = Vector3.zero;
+    private bool isVerticalMovingUpwards = true;
+
+
     private void Start()
     {
+        verticalStartPosition = transform.position;
 
+        verticalDestinationPosition = verticalStartPosition;
+        verticalDestinationPosition.y += verticalMovementRange;
     }
- 
+
     private void Update()
     {
-     transform.Rotate(3f, 0f ,0f);   
+        transform.Rotate(rotationSpeed, 0f ,0f);
+
+        transform.position = Vector3.MoveTowards(transform.position, verticalDestinationPosition, verticalMovementSpeed * Time.deltaTime);
+
+        if (Mathf.Approximately(transform.position.y, verticalDestinationPosition.y))
+        {
+            if (isVerticalMovingUpwards)
+            {
+                verticalDestinationPosition.y = verticalStartPosition.y - verticalMovementRange;
+            }
+            else
+            {
+                verticalDestinationPosition.y = verticalStartPosition.y + verticalMovementRange;
+            }
+
+            isVerticalMovingUpwards = !isVerticalMovingUpwards;
+        }
     }
 
 
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -8,11 +9,17 @@ public class GameController : MonoBehaviour
     public GameObject pointObject;
 
 
-    
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
 
     public void AddNewPoint()
     {
-        Vector3 position = new Vector3(Random.Range(-areaSize/2f, areaSize/2f), 0.5f, Random.Range(-areaSize/2f, areaSize/2f));
+        Vector3 position = new Vector3(Random.Range(-areaSize/2f, areaSize/2f), 0.95f, Random.Range(-areaSize/2f, areaSize/2f));
         Instantiate(pointObject, position, pointObject.transform.rotation);
    
     }
